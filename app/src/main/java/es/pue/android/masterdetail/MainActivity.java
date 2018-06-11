b9b8d5acdf1588610073c633da7c28e15e093d1f
Manel <yo@manelperez.com>
2018-06-11 13:37:46 +0200

    Capture click on fragment button and coordination in the activity

--- app/src/main/java/es/pue/android/masterdetail/MainActivity.java
+++ app/src/main/java/es/pue/android/masterdetail/MainActivity.java
@@ -1,13 +1,27 @@
 package es.pue.android.masterdetail;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import es.pue.android.masterdetail.fragments.DataFragment;
+
+public class MainActivity extends AppCompatActivity implements DataFragment.DataListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void sendData(String data) {
+        Intent i = new Intent(this, DetailActivity.class);
+        i.putExtra("data", data);
+        startActivity(i);
     }
 }
--- app/src/main/java/es/pue/android/masterdetail/fragments/DataFragment.java
+++ app/src/main/java/es/pue/android/masterdetail/fragments/DataFragment.java
@@ -1,11 +1,15 @@
 package es.pue.android.masterdetail.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import es.pue.android.masterdetail.R;
 
@@ -13,18 +17,50 @@ import es.pue.android.masterdetail.R;
  * A simple {@link Fragment} subclass.
  */
 public class DataFragment extends Fragment {
-
+    private EditText etData;
+    private DataListener callback;
 
     public DataFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            callback = (DataListener) context;
+        } catch(Exception e) {
+            throw new ClassCastException("Error in conversion");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_data, container, false);
+
+        etData = view.findViewById(R.id.etData);
+
+        Button btnSend = view.findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(getClickSendButtonListener());
+
+        return view;
     }
 
+    @NonNull
+    private View.OnClickListener getClickSendButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dataToSend = etData.getText().toString();
+                callback.sendData(dataToSend);
+            }
+        };
+    }
+
+    public interface DataListener {
+        void sendData(String data);
+    }
 }
