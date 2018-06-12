015276b9397618d103c2ca78a70a84b4075c4b46
Manel <yo@manelperez.com>
2018-06-12 09:41:45 +0200

    Manage multipanel

--- app/src/main/java/es/pue/android/masterdetail/MainActivity.java
+++ app/src/main/java/es/pue/android/masterdetail/MainActivity.java
@@ -9,19 +9,39 @@ import android.view.View;
 import android.widget.Button;
 
 import es.pue.android.masterdetail.fragments.DataFragment;
+import es.pue.android.masterdetail.fragments.DetailFragment;
 
 public class MainActivity extends AppCompatActivity implements DataFragment.DataListener {
+
+    private boolean isMultiPanel = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isMultiPanel = isMultiPanel();
     }
 
     @Override
     public void sendData(String data) {
-        Intent i = new Intent(this, DetailActivity.class);
-        i.putExtra("data", data);
-        startActivity(i);
+        /**
+         * If we are in portrait, we send data to new activity.
+         * If we are in landscape we are in multipanel scenario, so we can send data directly to
+         * the detail fragment.
+         */
+        if (isMultiPanel) {
+            DetailFragment detailFragment = (DetailFragment) getFragmentManager()
+                    .findFragmentById(R.id.detailFragment);
+            detailFragment.renderData(data);
+        } else {
+            Intent i = new Intent(this, DetailActivity.class);
+            i.putExtra("data", data);
+            startActivity(i);
+        }
+    }
+
+    private boolean isMultiPanel() {
+        return (null != getFragmentManager().findFragmentById(R.id.detailFragment));
     }
 }
